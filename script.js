f7fb6d0103cd4d50b9fdd22f478fc36d375d69f4
MahdiHosseini <MahdiHosseinigig@gmail.com>
2023-09-11 12:22:08 -0400

    Add | add script javascript variable

---
+++ script.js
@@ -0,0 +1,38 @@
+let rightEye = document.querySelector('#righteye')
+let leftEye = document.querySelector('#lefteye')
+let userInput = document.querySelector('#userInput')
+let passInput = document.querySelector('#passInput')
+
+let leftEyeTop = 75;
+let leftEyeLeft = 110;
+let leftEyePaddingLeft = 0;
+let leftEyePaddingTop = 0;
+
+let rightEyeTop = 75;
+let rightEyeLeft = 175;
+let rightEyePaddingLeft = 0;
+let rightEyePaddingTop = 0;
+
+
+const userInputFocus = () =>{
+    
+}
+const userInputBlur = () =>{
+    
+}
+const userKeyHandler = () =>{
+    
+}
+const passInputFocus = () =>{
+    
+}
+const passInputBlur = () =>{
+    
+}
+
+userInput.addEventListener('fucus', userInputFocus)
+userInput.addEventListener('blur', userInputBlur)
+userInput.addEventListener('keypress', userKeyHandler)
+
+userInput.addEventListener('fucus', passInputFocus)
+userInput.addEventListener('blur', passInputBlur)
